70a2de7255e40735956f22e695c55b54119b82e7
MasatakaIshii <masaradio78@gmail.com>
2022-10-14 01:57:27 +0200

    error logout when arguments are not correct

--- src/main/java/fr/soat/mi/katasgcib/Application.java
+++ src/main/java/fr/soat/mi/katasgcib/Application.java
@@ -14,14 +14,15 @@ public class Application {
     final public static String ACCOUNTS_FILE = "accounts.csv";
 
     public static void main(String[] args) {
+        var logger = new ConsoleLogger();
         if (args.length < 2) {
-            throw new IllegalArgumentException("Not enough arguments");
+            logger.err("Not enough arguments");
         }
         var action = args[0];
         if (action.equals("deposit") && args.length != 3) {
-            throw new IllegalArgumentException("Deposit need 3 arguments");
+            logger.err("Deposit need 3 arguments");
         }
-        var logger = new ConsoleLogger();
+
         var fileReader = new DefaultFileReader(logger);
         var fileWriter = new DefaultFileWriter();
         var accountParser = new AccountParserImpl(fileReader, fileWriter);
